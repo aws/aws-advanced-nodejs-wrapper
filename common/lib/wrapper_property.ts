b4d5fde54a2a47c89f1a8abf124667ef01cd3b9f
Crystal Liang <crystal.liang@improving.com>
2024-03-11 11:56:53 -0700

    fix: add port to persisting properties in removeWrapperProperties method

--- common/lib/wrapper_property.ts
+++ common/lib/wrapper_property.ts
@@ -98,7 +98,7 @@ export class WrapperProperties {
 
   static removeWrapperProperties<T>(config: T): T {
     const copy = Object.assign({}, config);
-    const persistingProperties = [WrapperProperties.USER.name, WrapperProperties.PASSWORD.name, WrapperProperties.DATABASE.name];
+    const persistingProperties = [WrapperProperties.USER.name, WrapperProperties.PASSWORD.name, WrapperProperties.DATABASE.name, WrapperProperties.PORT.name];
 
     Object.values(WrapperProperties).forEach((prop) => {
       if (prop instanceof WrapperProperty) {
