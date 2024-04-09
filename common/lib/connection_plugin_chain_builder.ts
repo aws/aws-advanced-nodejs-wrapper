b9ec8fe326d39823b5b23b91de152887d15179dc
jasonlamz <161892786+jasonlamz@users.noreply.github.com>
2024-04-09 10:24:47 -0700

    feat: connection time plugin (#17)

--- common/lib/connection_plugin_chain_builder.ts
+++ common/lib/connection_plugin_chain_builder.ts
@@ -22,6 +22,7 @@ import { WrapperProperties } from "./wrapper_property";
 import { AwsWrapperError } from "./utils/aws_wrapper_error";
 import { Messages } from "./utils/messages";
 import { DefaultPlugin } from "./plugins/default_plugin";
+import { ConnectTimePluginFactory } from "./plugins/connect_time_plugin";
 import { AwsSecretsManagerPluginFactory } from "./authentication/aws_secrets_manager_plugin";
 
 export class PluginFactoryInfo {}
@@ -34,6 +35,7 @@ export class ConnectionPluginChainBuilder {
 
   static readonly PLUGIN_FACTORIES = new Map<string, FactoryClass>([
     ["iam", IamAuthenticationPluginFactory],
+    ["connectTime", ConnectTimePluginFactory],
     ["secretsManager", AwsSecretsManagerPluginFactory],
     ["failover", FailoverPluginFactory]
   ]);
