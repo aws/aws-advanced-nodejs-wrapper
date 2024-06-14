6b1bc941b5acece52baaccf69fafb19935f73d40
jasonlamz <lijjaso@amazon.com>
2024-06-14 16:30:46 -0700

    unit test

--- tests/unit/okta_credentials_provider.test.ts
+++ tests/unit/okta_credentials_provider.test.ts
@@ -14,7 +14,7 @@
   limitations under the License.
 */
 
-import { anything, instance, spy } from "ts-mockito";
+import { instance, spy } from "ts-mockito";
 import { WrapperProperties } from "aws-wrapper-common-lib/lib/wrapper_property";
 import { readFileSync } from "fs";
 import { OktaCredentialsProviderFactory } from "aws-wrapper-common-lib/lib/plugins/federated_auth/okta_credentials_provider_factory";
@@ -24,29 +24,44 @@ const username = "someFederatedUsername@example.com";
 const password = "ec2amazab3cdef";
 const endpoint = "example.okta.com";
 const applicationId = "example.okta.com";
-const props = new Map<string, any>();
 
 const sessionTokenJson = JSON.parse(readFileSync("tests/unit/resources/okta/session-token.json", "utf8"));
 const samlAssertionHtml = readFileSync("tests/unit/resources/okta/saml-assertion.html", "utf8");
 const expectedSamlAssertion = readFileSync("tests/unit/resources/okta/assertion.txt", "utf8");
+const expectedSessionToken = readFileSync("tests/unit/resources/okta/expected-session-token.txt", "utf8");
 
+const postResponse = { data: sessionTokenJson };
+const getResponse = { data: samlAssertionHtml };
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 jest.mock("axios");
 
 describe("oktaCredentialsProviderTest", () => {
-  it("testGetSamlAssertion", async () => {
+  let props: Map<string, any>;
+
+  beforeEach(() => {
+    props = new Map<string, any>();
     WrapperProperties.IDP_ENDPOINT.set(props, endpoint);
     WrapperProperties.APP_ID.set(props, applicationId);
     WrapperProperties.IDP_USERNAME.set(props, username);
     WrapperProperties.IDP_PASSWORD.set(props, password);
+  });
 
-    const postResponse = { data: sessionTokenJson };
-    const getResponse = { data: samlAssertionHtml };
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    const mockedAxios = axios as jest.Mocked<typeof axios>;
-    mockedAxios.request.mockResolvedValueOnce(postResponse).mockResolvedValueOnce(getResponse);
-
+  it("testGetSessionToken", async () => {
+    mockedAxios.request.mockResolvedValueOnce(postResponse);
     const spyCredentialsFactory = spy(new OktaCredentialsProviderFactory());
+    const sessionToken = await instance(spyCredentialsFactory).getSessionToken(props);
 
+    expect(sessionToken).toBe(expectedSessionToken);
+  });
+
+  it("testGetSamlAssertion", async () => {
+    mockedAxios.request.mockResolvedValueOnce(postResponse).mockResolvedValueOnce(getResponse);
+    const spyCredentialsFactory = spy(new OktaCredentialsProviderFactory());
     const samlAssertion = await instance(spyCredentialsFactory).getSamlAssertion(props);
 
     expect(samlAssertion).toBe(expectedSamlAssertion);
