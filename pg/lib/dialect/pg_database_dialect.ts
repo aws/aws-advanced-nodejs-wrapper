aaa8f0883da8066f24002f13862c6a48d3524385
joyc-bq <joyc@bitquilltech.com>
2024-08-15 09:35:36 -0700

    query for pg

--- pg/lib/dialect/pg_database_dialect.ts
+++ pg/lib/dialect/pg_database_dialect.ts
@@ -79,17 +79,14 @@ export class PgDatabaseDialect implements DatabaseDialect {
 
   async isClientValid(targetClient: ClientWrapper): Promise<boolean> {
     try {
-      return await ClientUtils.queryWithTimeout(
-        targetClient.client
-          .query("SELECT 1")
-          .then(() => {
-            return true;
-          })
-          .catch(() => {
-            return false;
-          }),
-        targetClient.properties
-      );
+      return await targetClient.client
+        .query("SELECT 1")
+        .then(() => {
+          return true;
+        })
+        .catch(() => {
+          return false;
+        });
     } catch (error) {
       return false;
     }
