68f6898ec7568dc9e9d54d3932c7864241f8e3bf
joyc-bq <joyc@bitquilltech.com>
2024-08-18 11:03:18 -0700

    query timeout time

--- common/lib/wrapper_property.ts
+++ common/lib/wrapper_property.ts
@@ -66,7 +66,7 @@ export class WrapperProperties {
   static readonly INTERNAL_QUERY_TIMEOUT = new WrapperProperty<number>(
     "internal_query_timeout",
     "Timeout in milliseconds for the driver to execute internal queries, such as the query for cluster topology",
-    20000
+    40000
   );
 
   static readonly TRANSFER_SESSION_STATE_ON_SWITCH = new WrapperProperty<boolean>(
