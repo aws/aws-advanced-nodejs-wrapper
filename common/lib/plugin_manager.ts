5c23e62b4277d0969afda722c6dafc1bf44ca84f
joyc-bq <joyc@bitquilltech.com>
2025-05-09 14:04:13 -0700

    plugin chain cache

--- common/lib/plugin_manager.ts
+++ common/lib/plugin_manager.ts
@@ -63,7 +63,7 @@ class PluginChain<T> {
 }
 
 export class PluginManager {
-  private static readonly PLUGIN_CHAIN_CACHE = new Map<[string, HostInfo], PluginChain<any>>();
+  private static readonly PLUGIN_CHAIN_CACHE = new Map<string, PluginChain<any>>();
   private static readonly STRATEGY_PLUGIN_CHAIN_CACHE = new Map<ConnectionPlugin[], Set<ConnectionPlugin>>();
   private static readonly ALL_METHODS: string = "*";
   private static readonly CONNECT_METHOD = "connect";
@@ -189,10 +189,10 @@ export class PluginManager {
     pluginFunc: PluginFunc<T>,
     methodFunc: () => Promise<T>
   ): Promise<T> {
-    let chain = PluginManager.PLUGIN_CHAIN_CACHE.get([methodName, hostInfo]);
+    let chain = PluginManager.PLUGIN_CHAIN_CACHE.get(JSON.stringify([methodName, hostInfo.host]));
     if (!chain) {
       chain = this.makeExecutePipeline(hostInfo, props, methodName, methodFunc);
-      PluginManager.PLUGIN_CHAIN_CACHE.set([methodName, hostInfo], chain);
+      PluginManager.PLUGIN_CHAIN_CACHE.set(JSON.stringify([methodName, hostInfo.host]), chain);
     }
     return chain.execute(pluginFunc, methodFunc);
   }
