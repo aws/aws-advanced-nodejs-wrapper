ce7fd07058cf42ad491ef739a16dd3d7e69c1d16
joyc-bq <joyc@bitquilltech.com>
2025-06-04 14:21:39 -0700

    import

--- tests/integration/container/tests/read_write_splitting.test.ts
+++ tests/integration/container/tests/read_write_splitting.test.ts
@@ -31,7 +31,6 @@ import { HostInfo } from "../../../../common/lib/host_info";
 import { PluginManager } from "../../../../common/lib";
 import { RdsHostListProvider } from "../../../../common/lib/host_list_provider/rds_host_list_provider";
 import { PluginService } from "../../../../common/lib/plugin_service";
-import { readFileSync } from "fs";
 
 const itIf =
   !features.includes(TestEnvironmentFeatures.PERFORMANCE) &&
