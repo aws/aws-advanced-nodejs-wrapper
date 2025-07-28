7aa15c8f4273de43c83a230d087d67511b45c9af
Karen Chen <64801825+karenc-bq@users.noreply.github.com>
2025-07-28 14:33:18 -0700

    fix: ensure driver helper for integration tests can handle multi-az instance deployments correctly

--- tests/integration/container/tests/utils/driver_helper.ts
+++ tests/integration/container/tests/utils/driver_helper.ts
@@ -69,6 +69,7 @@ export class DriverHelper {
           default:
             throw new Error("invalid engine");
         }
+      case DatabaseEngineDeployment.RDS_MULTI_AZ_INSTANCE:
       case DatabaseEngineDeployment.RDS_MULTI_AZ_CLUSTER:
         switch (engine) {
           case DatabaseEngine.PG:
